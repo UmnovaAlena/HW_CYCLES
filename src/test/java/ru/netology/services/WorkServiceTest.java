090821e64b4d5a5ccc1eac4864d1129d6cab6bf0
Alena <umnovaalena@rambler.ru>
2023-04-13 23:34:25 +0400

    Added commit

--- src/test/java/ru/netology/services/WorkServiceTest.java
+++ src/test/java/ru/netology/services/WorkServiceTest.java
@@ -2,28 +2,46 @@ package ru.netology.services;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
 import ru.netology.services.WorkService;
 
 public class WorkServiceTest {
 
-    @Test
-    public void shouldCalcWorkRelax() {
+    @ParameterizedTest
+    //  @CsvSource({
+    //          "3,10000,3000,20000",
+    //          "2,100000,60000,150000"
+    // })
+
+    @CsvFileSource(files = "src/test/resources/relax.csv")
+    public void testWorkRelax(int expected, int income, int expenses, int threshold) {
         WorkService service = new WorkService();
 
-        int expected = 3;
-        int actual = service.calculate(10000, 3000, 20000);
+        int actual = service.calculate(income, expenses, threshold);
 
         Assertions.assertEquals(expected, actual);
     }
 
-    @Test
-    public void shouldCalcRelaxWork() {
-        WorkService service = new WorkService();
+    // @Test
+    // public void shouldCalcWorkRelax() {
+    // WorkService service = new WorkService();
 
-        int expected = 2;
-        int actual = service.calculate(100000, 60000, 150000);
+    // int expected = 3;
+    // int actual = service.calculate(10000, 3000, 20000);
 
-        Assertions.assertEquals(expected, actual);
-    }
+    // Assertions.assertEquals(expected, actual);
+    // }
+
+    // @Test
+    // public void shouldCalcRelaxWork() {
+    // WorkService service = new WorkService();
+
+    // int expected = 2;
+    // int actual = service.calculate(100000, 60000, 150000);
+
+    // Assertions.assertEquals(expected, actual);
+    //}
 
 }
